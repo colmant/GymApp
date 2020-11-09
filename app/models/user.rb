4a3346a93155b94985242164dc93ad1e96e2b067
MickeySacks <52339317+MickeySacks@users.noreply.github.com>
2020-11-08 22:39:52 -0500

    Delete user.rb

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # :recoverable
-  devise :database_authenticatable, :registerable,
-        :rememberable, :validatable
-end
