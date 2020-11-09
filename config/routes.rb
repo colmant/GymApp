01c4317ae5f4092b8480ef19f354ba0adf3be145
Ubuntu <ubuntu@ip-172-31-58-104.ec2.internal>
2020-11-09 01:25:26 +0000

    user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-#   devise_for :users
-#     resources :user, :only => [:index, :show, :new, :create] #unsure what our resource will be named, gym is a place holder
-#     root :to => "user#index"
+
 end
