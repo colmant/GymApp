cd6c8991d81dc8dd14b76e294a1b6f0f24a855bc
Ubuntu <ubuntu@ip-172-31-20-35.ec2.internal>
2020-11-21 21:34:23 +0000

    added has_one association to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_042024) do
+ActiveRecord::Schema.define(version: 2020_11_21_212852) do
 
   create_table "tickets", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_11_09_042024) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tickets", "users"
 end
