962f982e62f3821b4d6479d4bf9bcecb60052d3e
Ubuntu <ubuntu@ip-172-31-22-248.ec2.internal>
2020-12-06 21:39:37 +0000

    started gyms_controller

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -9,11 +9,7 @@ class GymsController < ApplicationController
     
     def update
     
-        flash[:notice] = "#{params[:submit]}"
         
-        puts params.inspect
-        puts "Hello world"
-        puts params[:id]
         @gym = Gym.find_by(name: "Trudy")
         if params[:id] == "1"
             @gym.subtract_top_floor()
