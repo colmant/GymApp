db93da9f56339b01fe8fe446a1eabee945aca0a8
Ubuntu <ubuntu@ip-172-31-58-104.ec2.internal>
2020-12-07 22:53:39 +0000

    building controller and model functionality

--- app/models/trudy_trend.rb
+++ app/models/trudy_trend.rb
@@ -1,2 +1,11 @@
 class TrudyTrend < ApplicationRecord
+    def self.sorted_by(field) #need to add default sorting by current day/date?
+        if TrudyTrend.column_names.include?(field.to_s) #Date.today.strftime("%A")
+            return Product.order(field.to_s)
+        elsif field.to_s =="age"
+            return Product.order('minimum_age_appropriate')
+        else
+            return Product.order(:name)
+        end
+    end
 end
