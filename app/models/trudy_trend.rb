85909a5d0dda2b8ab0fd88947959a316d10ff34b
Ubuntu <ubuntu@ip-172-31-58-104.ec2.internal>
2020-12-08 00:06:04 +0000

    fixed conflicts

--- app/models/trudy_trend.rb
+++ app/models/trudy_trend.rb
@@ -1,11 +1,5 @@
 class TrudyTrend < ApplicationRecord
-<<<<<<< HEAD
-    def change 
-        create_table :times do |t|
-            t.timestamps
-        end 
-    end
-=======
+
     def self.sorted_by(field) #need to add default sorting by current day/date?
         if TrudyTrend.column_names.include?(field.to_s) #Date.today.strftime("%A")
             return TrudyTrend.order(field.to_s)
@@ -23,5 +17,4 @@ class TrudyTrend < ApplicationRecord
     def get_average_queue_size
         @trudy_trend = TrudyTrend.find_by(name: Date.today.strftime("%A")).average_queue_size
     end
->>>>>>> b5fc4fd8e20b01db97431ec5e9886162fa559d7e
 end
